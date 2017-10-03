c5220f2a7e098b1f08d2bdd9fe036984e9655a2e
GitGatCat <petterelvevoll@gmail.com>
2017-10-03 15:26:00 +0200

    kjøretøy

---
+++ Vehicle.py
@@ -0,0 +1,21 @@
+class Vehicle(object):
+    def __init__(self, x, y, speed):
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.lastMove = time.time()
+
+    def move(self, node):
+        if self.x == node.x:
+            pass
+        elif self.x > node.x:
+            self.x -= self.speed
+        else:
+            self.x += self.speed
+
+        if self.y == node.y:
+            pass
+        elif self.y < node.y:
+            self.y += self.speed
+        else:
+            self.y -= self.speed
