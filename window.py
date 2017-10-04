99b9a78b72a1c32a26fe5f5dab87b4f676bb5264
GitGatCat <petterelvevoll@gmail.com>
2017-10-04 18:57:41 +0200

    no longer Node.py

--- window.py
+++ window.py
@@ -1,52 +1,54 @@
-class Node(object):
+import sys, pygame
+from Node import *
+from Vehicle import *
 
-    def __init__(self,x,y):
-        self.matrix = []
-        self.x = x
-        self.y = y
+pygame.init()
 
-    def getPos(self):
-        return self.x, self.y
+size = width, height = 300,300
 
-    def add(self, node):
-        self.matrix.append([node])
+screen = pygame.display.set_mode()
+pygame.display.toggle_fullscreen()
 
-    def get_row(self, node):
-        for x in self.matrix:
-            if node in x:
-                return x
+surface = pygame.display.get_surface()
+black = 0,0,0
+screen.fill(black)
+lineColor = 120,120,10
+lastPos = 0,0
 
-    def add_constrain(self, node, constrain):
-        if constrain not in Node.get_row(self, node):
-            self.get_row(node).append(constrain)
+nodes = []
+nodes.append(Node(10,10))
+nodes.append(Node(100,10))
+nodes.append(Node(50,50))
+nodes[0].add(nodes[1])
+nodes[1].add(nodes[2])
 
-    def is_satisfied(self, node):
-        for x in self.get_row(node):
-            if isinstance(x, Constraint):
-                if not x.is_satisfied():
-                    return False
-        return True
 
-""" 
-    holder for functions and variables for boolean logic
-"""
-class Constraint(object):
+car = Vehicle(10, 10, 5)
+target = 1
 
-    def __init__(self):
-        self.constraints = []
+black = 0,0,0
+while 1:
+    surface.fill(black)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pygame.draw.line(surface, lineColor, lastPos, pygame.mouse.get_pos(), 4)
+            lastPos = pygame.mouse.get_pos()
 
-    def add_constraint(self, constraint):
-        self.constraints.append(constraint)
+    for n in nodes:
+        pygame.draw.circle(surface, lineColor, n.getPos(), 20, 4)
 
-    def is_satisfied(self):
-        for x in self.constraints:
-            if callable(x):
-                if not x(self):
-                    return False
-            else:
-                if not x:
-                    return False
-        return True
+    for n in nodes:
+        for c in n.matrix:
+            if len(c) > 0:
+                pygame.draw.line(surface, lineColor, n.getPos(), c[0].getPos(), 4)
 
-    def green_light(self):
-        return True
+    if time.time() - t > 0.1:
+        if car.x == nodes[target].getPos()[0] and car.y == nodes[target].getPos()[1]:
+            target = 2
+        car.move(nodes[target])
+        t = time.time()
+    pygame.draw.circle(surface, lineColor, (car.x,car.y), 40, 4)
+
+    pygame.display.flip()
